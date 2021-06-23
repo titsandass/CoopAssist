473f063eb9b33a31482a1f38e18618721019f422
Seunghwan17 <shchoi.vdrc@gmail.com>
2021-06-23 13:49:11 +0900

    update_by_SH

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+dpi = 200
+file_type = ".png"
 class CDMList():
     def __init__(self, cdms = []):
         self._CDMs = cdms
@@ -16,31 +18,31 @@ def read_tle(filename):
         tle_lines = tle.readlines()
 
         json_tle = list()
-        satellite = None
+        RSO = None
 
         for tle_line in tle_lines:
             if tle_line.startswith('0'):
                 sat_name = tle_line.split()
                 del(sat_name[0])
 
-                if satellite is not None:
-                    json_tle.append(satellite)
+                if RSO is not None:
+                    json_tle.append(RSO)
 
-                satellite = dict()
-                satellite_name = str()
+                RSO = dict()
+                RSO_name = str()
 
                 for name in sat_name:
-                    satellite_name += name + " "
+                    RSO_name += name + " "
 
-                satellite['SATNAME'] = satellite_name[:-1]
+                RSO['SATNAME'] = RSO_name[:-1]
 
             elif tle_line.startswith('1'):
-                satellite['s'] = tle_line
+                RSO['s'] = tle_line
 
             elif tle_line.startswith('2'):
-                satellite['t'] = tle_line
+                RSO['t'] = tle_line
 
-                satellite['SAT_ID'] = tle_line.split()[1].zfill(5)
+                RSO['SAT_ID'] = tle_line.split()[1].zfill(5)
 
     return json_tle
 
@@ -92,24 +94,24 @@ def read_ppdb(filename):
     return ppdbs
 
 
-def generate_satellite(tle):
+def generate_RSO(tle):
     from sgp4.earth_gravity import wgs72
     from sgp4.io import twoline2rv
     import pickle
 
-    satellites = dict()
+    RSOs = dict()
 
-    for satellite in tle:
-        sat_id = satellite['SAT_ID']
-        satellites[sat_id] = (satellite['SATNAME'], twoline2rv(satellite["s"], satellite["t"], wgs72))
+    for RSO in tle:
+        sat_id = RSO['SAT_ID']
+        RSOs[sat_id] = (RSO['SATNAME'], twoline2rv(RSO["s"], RSO["t"], wgs72))
 
-    with open("./CoopAssist/satellite.pickle", "wb") as f:
-        pickle.dump(satellites, f)
+    with open("./COOP_data/RSO.pickle", "wb") as f:
+        pickle.dump(RSOs, f)
 
-    return satellites
+    return RSOs
 
 
-def compare_SGP4_N_PPDB(satellites, PPDBs, Radius_of_Earth):
+def compare_SGP4_N_PPDB(RSOs, PPDBs, RADIUS_OF_EARTH):
     import datetime as dt
     import numpy as np
 
@@ -122,17 +124,17 @@ def compare_SGP4_N_PPDB(satellites, PPDBs, Radius_of_Earth):
     not_in_tle_IDs = set()
 
     for PPDB in PPDBs:
-        if PPDB["SAT1ID"].zfill(5) not in satellites.keys():
+        if PPDB["SAT1ID"].zfill(5) not in RSOs.keys():
             not_in_tle_IDs.add(PPDB["SAT1ID"].zfill(5))
             print(PPDB["SAT1ID"].zfill(5) + "is not is TLE")
             continue
-        elif PPDB["SAT2ID"].zfill(5) not in satellites.keys():
+        elif PPDB["SAT2ID"].zfill(5) not in RSOs.keys():
             not_in_tle_IDs.add(PPDB["SAT2ID"].zfill(5))
             print(PPDB["SAT2ID"].zfill(5) + "is not is TLE")
             continue
 
-        sat1 = satellites[PPDB["SAT1ID"].zfill(5)][1]
-        sat2 = satellites[PPDB["SAT2ID"].zfill(5)][1]
+        sat1 = RSOs[PPDB["SAT1ID"].zfill(5)][1]
+        sat2 = RSOs[PPDB["SAT2ID"].zfill(5)][1]
 
         PPDB_TCA = dt.datetime.strptime(PPDB["TCA"], "%Y-%m-%dT%H:%M:%S.%f")
 
@@ -149,9 +151,9 @@ def compare_SGP4_N_PPDB(satellites, PPDBs, Radius_of_Earth):
         result["PPDB_DCA"]              = PPDB["MinDistance"]
         result["SGP4_DCA@PPDB_TCA"]     = PPDB_DCA
         result["SGP4_SAT1_POS@PPDB_TCA"]= PPDB_sat1_pos
-        result["SGP4_SAT1_ALT@PPDB_TCA"]= np.linalg.norm(np.asarray(PPDB_sat1_pos)) - Radius_of_Earth
+        result["SGP4_SAT1_ALT@PPDB_TCA"]= np.linalg.norm(np.asarray(PPDB_sat1_pos)) - RADIUS_OF_EARTH
         result["SGP4_SAT2_POS@PPDB_TCA"]= PPDB_sat2_pos
-        result["SGP4_SAT2_ALT@PPDB_TCA"]= np.linalg.norm(np.asarray(PPDB_sat2_pos)) - Radius_of_Earth
+        result["SGP4_SAT2_ALT@PPDB_TCA"]= np.linalg.norm(np.asarray(PPDB_sat2_pos)) - RADIUS_OF_EARTH
 
         total_results.append(result)
     return total_results, not_in_tle_IDs
@@ -196,14 +198,15 @@ def save_result(result_filename, total_results, not_in_tle_IDs):
         pickle.dump(total_results, f)
 
 
-def calculate_and_save_conjunction_histogram(total_results, cutoff_distance, distance_interval, bin_interval, save_individual):
+def calculate_and_save_conjunction_histogram(total_results, threshold_distance, distance_interval, bin_interval, save_individual):
     import numpy as np
     import matplotlib.pyplot as plt
 
-    distances = np.arange(distance_interval, cutoff_distance + distance_interval, distance_interval)
+    distances = np.arange(distance_interval, threshold_distance + distance_interval, distance_interval)
     histogram = {distance : list() for distance in distances}
     bins = [i*bin_interval for i in range(int(2000/bin_interval))]
-    
+
+
     for result in total_results:
         for distance in distances:
             if float(result["PPDB_DCA"]) <= distance:
@@ -220,30 +223,45 @@ def calculate_and_save_conjunction_histogram(total_results, cutoff_distance, dis
             "#f15f01",
             "#eb0909"]
 
-    plt.rcParams["figure.figsize"] = (5, 10)
-    plt.tight_layout()
+    plt.rcParams["figure.figsize"] = (8, 8)
     plt.figure()
 
     for i, distance in enumerate(reversed(distances)):
         if save_individual:
-            plt.figure()
+            # plt.ylim(0,2000)
+            plt.title("Conjunction Distribution at Threshold")
+            plt.xlabel("# Conjunctions")
+            plt.ylabel("Altitude (km)")
             plt.hist(x=histogram[distance], bins=bins, orientation="horizontal", color=colors[i], label=str(distance)+" km")
-            plt.legend()
-            plt.savefig("./CoopAssist/"+str(distance)+"_histogram.png", dpi=200)
+            plt.legend(title="Threshold")
+            plt_file = "./COOP_data/output/"+str(distance)+"_histogram"+file_type
+            if os.path.isfile(plt_file):
+                os.remove(plt_file)
+            plt.tight_layout()
+            plt.savefig(plt_file, dpi=dpi)
+            plt.clf()
         else:
+            plt.ylim(0,2000)
+            plt.title("Conjunction Distribution at Threshold")
+            plt.xlabel("# Conjunctions")
+            plt.ylabel("Altitude (km)")
             plt.hist(x=histogram[distance], bins=bins, orientation="horizontal", color=colors[i], label=str(distance)+" km")
 
     # plt.hlines(860, 0, 8000, colors="black", linestyles="dashed")
     plt.legend(title="Threshold")
-    plt.xlabel("#Conjunctions")
-    plt.ylabel("Heights (km)")
 
     if not save_individual:
-        plt.savefig("./CoopAssist/total_histogram.png", dpi=200)
+        plt_file = "./COOP_data/output/total_histogram"+file_type
+        if os.path.isfile(plt_file):
+            os.remove(plt_file)
+        
+        plt.tight_layout()
+        plt.savefig(plt_file, dpi=dpi)
+        plt.clf()
     
     return histogram
 
-def draw_conjunction_polarplane_2D(total_results, Radius_of_Earth, show_full_quadrant):
+def draw_conjunction_polarplane_2D(total_results, RADIUS_OF_EARTH, show_full_quadrant):
     import numpy as np
     import math
     import matplotlib.pyplot as plt
@@ -256,39 +274,45 @@ def draw_conjunction_polarplane_2D(total_results, Radius_of_Earth, show_full_qua
     for result in total_results:
         sat1_pos    = np.asarray(result["SGP4_SAT1_POS@PPDB_TCA"])
         x, y, z     = sat1_pos
-        radius      = np.linalg.norm(sat1_pos) - Radius_of_Earth
+        radius      = np.linalg.norm(sat1_pos) - RADIUS_OF_EARTH
         theta       = math.atan(z/np.linalg.norm(sat1_pos[:2]))
 
         if show_full_quadrant:
             theta = _reflect_for_full_quadrant(x, y, z, theta)
-        # if show_full_quadrant:
-        #     if y > 0 and z > 0:
-        #         pass
-        #     elif y < 0 and z > 0:
-        #         theta = math.radians(180) - theta
-        #     elif y < 0 and z < 0:
-        #         theta = math.radians(180) - theta
-        #     elif y > 0 and z < 0:
-        #         pass
-        #     else:
-        #         raise ValueError("?")
 
         Rs.append(radius)
         Thetas.append(theta)
-    
-    # colors = []
+
+        sat2_pos    = np.asarray(result["SGP4_SAT2_POS@PPDB_TCA"])
+        x, y, z     = sat2_pos
+        radius      = np.linalg.norm(sat2_pos) - RADIUS_OF_EARTH
+        theta       = math.atan(z/np.linalg.norm(sat2_pos[:2]))
+
+        if show_full_quadrant:
+            theta = _reflect_for_full_quadrant(x, y, z, theta)
+
+        Rs.append(radius)
+        Thetas.append(theta)
+
 
     fig = plt.figure()
     ax = fig.add_subplot(projection="polar")
     ax.set_ylim(0,2000)
-    ax.set_yticks(np.arange(0,2100,100))
-    ax.scatter(Thetas, Rs, c=Rs, s=1, cmap='tab20b', alpha=1)
+    ax.set_yticks(np.arange(0,2100,200))
+    ax.set_yticks(np.arange(0,2100,100), minor = True)
+    ax.tick_params(axis='y', which = 'minor' , width = 1)
+    ax.scatter(Thetas, Rs, color="#33AA33", s=0.1, alpha=0.5)
 
     if not show_full_quadrant:
         ax.set_thetamin(0)
         ax.set_thetamax(90)
 
-    plt.savefig("./CoopAssist/polar_conjunction.png", dpi=200)
+    plt_file = "./COOP_data/output/polar_conjunction"+file_type
+    if os.path.isfile(plt_file):
+        os.remove(plt_file)
+    plt.tight_layout()
+    plt.savefig(plt_file, dpi=dpi)
+    plt.clf()
 
     return Rs, Thetas
 
@@ -302,14 +326,13 @@ def _reflect_for_full_quadrant(x, y, z, theta):
         return theta
 
 
-def draw_satellite_histogram_at_time(satellites, str_time, Radius_of_Earth, bin_interval):
+def draw_RSO_histogram_at_time(RSOs, str_time, RADIUS_OF_EARTH, bin_interval):
     import datetime as dt
     import numpy as np
     import math
     import matplotlib.pyplot as plt
 
-    plt.rcParams["figure.figsize"] = (10, 10)
-    plt.tight_layout()
+    plt.rcParams["figure.figsize"] = (8, 8)
     plt.figure()
     
     colors=["#4f8f1a",
@@ -323,57 +346,152 @@ def draw_satellite_histogram_at_time(satellites, str_time, Radius_of_Earth, bin_
         "#f15f01",
         "#eb0909"]
 
-    # distances = np.arange(distance_interval, cutoff_distance + distance_interval, distance_interval)
+    # distances = np.arange(distance_interval, threshold_distance + distance_interval, distance_interval)
     # histogram = {distance : list() for distance in distances}
     bins = [i*bin_interval for i in range(int(2000/bin_interval))]
 
     time = dt.datetime.strptime(str_time, "%Y-%m-%dT%H:%M:%S.%f")
 
     altitudes = list()
-    for SATID in satellites.keys():
-        sat_pos, _  = satellites[SATID][1].propagate(time.year, time.month, time.day, time.hour, time.minute, time.second + time.microsecond/1000000)
-        altitude    = np.linalg.norm(np.asarray(sat_pos)) - Radius_of_Earth
+    for SATID in RSOs.keys():
+        sat_pos, _  = RSOs[SATID][1].propagate(time.year, time.month, time.day, time.hour, time.minute, time.second + time.microsecond/1000000)
+        altitude    = np.linalg.norm(np.asarray(sat_pos)) - RADIUS_OF_EARTH
 
         altitudes.append(altitude)
-
-    plt.hist(x=altitudes, bins=bins, orientation="horizontal", color=colors[0], ec="black")
-    plt.title("Satellite Altitude Distribution from TLE @" + str(time))
-    plt.xlabel("#Satellites")
+    
+    plt.hist(x=altitudes, bins=bins, orientation="horizontal", color="blue")
+    plt.title("RSO Altitude Distribution \n from TLE @" + str(time))
+    plt.ylim(0,2000)
+    plt.xlabel("# RSOs")
     plt.ylabel("Altitude (km)")
 
     # plt.show()
     str_time = time.strftime("%Y-%m-%dT%H%M%S.%f")
-    plt.savefig("./CoopAssist/satellite_altitude_distribution_" + str_time + ".png", dpi=200)
+
+    plt_file = "./COOP_data/output/RSO_altitude_distribution_" + str_time + file_type
+    if os.path.isfile(plt_file):
+        os.remove(plt_file)
+    plt.tight_layout()
+    plt.savefig(plt_file, dpi=dpi)
+    plt.clf()
+
+
+
+
+def draw_conjunctions_in_3D(total_results):
+    sat1_x_list = []
+    sat1_y_list = []
+    sat1_z_list = []
+    sat2_x_list = []
+    sat2_y_list = []
+    sat2_z_list = []
+
+    for result in total_results:
+
+        sat1_x, sat1_y, sat1_z = result["SGP4_SAT1_POS@PPDB_TCA"][0], result["SGP4_SAT1_POS@PPDB_TCA"][1], result["SGP4_SAT1_POS@PPDB_TCA"][2]
+        sat2_x, sat2_y, sat2_z = result["SGP4_SAT2_POS@PPDB_TCA"][0], result["SGP4_SAT2_POS@PPDB_TCA"][1], result["SGP4_SAT2_POS@PPDB_TCA"][2]
+
+        sat1_x_list.append(sat1_x)
+        sat1_y_list.append(sat1_y)
+        sat1_z_list.append(sat1_z)
+        sat2_x_list.append(sat2_x)
+        sat2_y_list.append(sat2_y)
+        sat2_z_list.append(sat2_z)
+
+    plt.rcParams["figure.figsize"] = (16, 17)
+    fig = plt.figure()
+
+    ax = fig.add_subplot(projection='3d')
+    ax.set_box_aspect((1,1,1))
+    ax.set_xlim(-6500, 6500)
+    ax.set_ylim(-6500, 6500)
+    # ax.set_yticks([])
+    ax.set_zlim(-6500, 6500)
+    ax.set_xlabel("X (km)", labelpad=30.0)
+    ax.set_ylabel("Y (km)", labelpad=30.0)
+    ax.set_zlabel("Z (km)", labelpad=30.0)
+    # for spine in ['top', 'right', 'left', 'bottom']:
+    #     ax.spines[spine].set_visible(True)
+
+    
+    ax.view_init(45, 45)
+
+    import numpy as np
+    r = 6378.14
+    pi = np.pi
+    cos = np.cos
+    sin = np.sin
+    phi, theta = np.mgrid[0.0:pi:50j, 0.0:2.0*pi:50j]
+    x = r*sin(phi)*cos(theta)
+    y = r*sin(phi)*sin(theta)
+    z = r*cos(phi)
+    ax.plot_surface(
+        x, y, z,  rstride=1, cstride=1, color='blue', alpha=0.5, linewidth=1)
+    
+    ax.scatter(sat1_x_list, sat1_y_list, sat1_z_list, 
+                color="#33AA33", marker='o', s=0.10, depthshade=True, alpha=1)
+    ax.scatter(sat2_x_list, sat2_y_list, sat2_z_list,
+                color="#33AA33", marker='o', s=0.10, depthshade=True, alpha=1)
+    
+    
+
+    plt.title("Conjunction Locations")
+    # plt.show()
+    plt_file = "./COOP_data/output/RSO_conjunction_in_3D" + file_type
+    if os.path.isfile(plt_file):
+        os.remove(plt_file)
+    plt.tight_layout()
+    plt.savefig(plt_file, dpi=dpi)
+    plt.clf()
+
 
 
 if __name__ == "__main__":
     import os
     import pickle
 
-    tle_filename    = "./CoopAssist/LEO_full_16709_210321_0800UTC.tle"
-    # cdm_filename    = "./CoopAssist/CDM-20210403.json"
-    ppdb_filename   = "./CoopAssist/PPDB2.txt"
-    result_filename = "./CoopAssist/result.txt"
+    import matplotlib.pyplot as plt 
+    SMALL_SIZE = 14
+    MEDIUM_SIZE = 16
+    BIGGER_SIZE = 16
+    plt.rcParams['font.family'] = 'Times New Roman'
+    plt.rc('font', size=SMALL_SIZE) # controls default text sizes 
+    plt.rc('axes', titlesize=BIGGER_SIZE) # fontsize of the axes title 
+    plt.rc('axes', labelsize=MEDIUM_SIZE) # fontsize of the x and y labels 
+    plt.rc('xtick', labelsize=SMALL_SIZE) # fontsize of the tick labels 
+    plt.rc('ytick', labelsize=SMALL_SIZE) # fontsize of the tick labels 
+    plt.rc('legend', fontsize=SMALL_SIZE) # legend fontsize 
+    plt.rc('figure', titlesize=BIGGER_SIZE) # fontsize of the figure title
 
-    satellite_filename = "./CoopAssist/satellite.pickle"
 
-    Radius_of_Earth             = 6378.1
+    tle_filename    = "./COOP_data/LEO_full_16709_210321_0800UTC.tle"
+    # cdm_filename    = "./COOP_data/CDM-20210403.json"
+    ppdb_filename   = "./COOP_data/PPDB2.txt"
+    result_filename = "./COOP_data/result.txt"
+
+    RSO_filename = "./COOP_data/RSO.pickle"
+
+    RADIUS_OF_EARTH             = 6378.1
+
     calculate_histogram         = True
-    calculate_conjunction_plane = True
-    show_full_quadrant          = True
+    calculate_conjunction_plane = False
+    show_full_quadrant          = False
     show_sat_alt_dist_histogram = True
-    cutoff_distance, distance_interval, bin_interval = 10.0, 1.0, 10.0
+    show_conjunctions_in_3D = False
 
-    if os.path.exists(result_filename) and os.path.exists(result_filename[:-4]+".pickle") and os.path.exists(satellite_filename):
+
+    threshold_distance, distance_interval, bin_interval = 10.0, 1.0, 25.0
+
+    if os.path.exists(result_filename) and os.path.exists(result_filename[:-4]+".pickle") and os.path.exists(RSO_filename):
         print("Found " + result_filename)
         print("Found " + result_filename[:-4]+".pickle")
-        print("Found " + satellite_filename)
+        print("Found " + RSO_filename)
 
         with open(result_filename[:-4]+".pickle", "rb") as f:
             total_results = pickle.load(f)
 
-        with open(satellite_filename, "rb") as f:
-            satellites = pickle.load(f)
+        with open(RSO_filename, "rb") as f:
+            RSOs = pickle.load(f)
     else:
         print("Read TLE " + tle_filename)
         TLEs = read_tle(tle_filename)
@@ -382,27 +500,33 @@ if __name__ == "__main__":
         print("Read PPDB " + ppdb_filename)
         PPDBs = read_ppdb(ppdb_filename)
 
-        print("Generating Satellites")
-        satellites = generate_satellite(TLEs)
+        print("Generating RSOs")
+        RSOs = generate_RSO(TLEs)
 
         print("Comparing SGP4 Propagation with PPDB")
-        total_results, not_in_tle_IDs = compare_SGP4_N_PPDB(satellites, PPDBs, Radius_of_Earth)
+        total_results, not_in_tle_IDs = compare_SGP4_N_PPDB(RSOs, PPDBs, RADIUS_OF_EARTH)
 
         print("Saving Result " + result_filename)
         save_result(result_filename, total_results, not_in_tle_IDs)
         print("Done")
 
     if calculate_histogram:
-        print("Cutoff Distance : {0}\nDistance Interval : {1}\nBin Interval : {2}\n".format(cutoff_distance, distance_interval, bin_interval))
+        print("Cutoff Distance : {0}\nDistance Interval : {1}\nBin Interval : {2}\n".format(threshold_distance, distance_interval, bin_interval))
         print("Calculating Conjunction Histogram")
 
+        save_individual_histogram = True
+        histogram = calculate_and_save_conjunction_histogram(total_results ,threshold_distance, distance_interval, bin_interval, save_individual_histogram)
         save_individual_histogram = False
-        histogram = calculate_and_save_conjunction_histogram(total_results ,cutoff_distance, distance_interval, bin_interval, save_individual_histogram)
+        histogram = calculate_and_save_conjunction_histogram(total_results ,threshold_distance, distance_interval, bin_interval, save_individual_histogram)
+
         print("Done")
 
     if calculate_conjunction_plane:
-        Rs, Thetas = draw_conjunction_polarplane_2D(total_results, Radius_of_Earth, show_full_quadrant)
+        Rs, Thetas = draw_conjunction_polarplane_2D(total_results, RADIUS_OF_EARTH, show_full_quadrant)
 
     if show_sat_alt_dist_histogram:
         str_time = "2021-3-26T0:0:0.000"
-        draw_satellite_histogram_at_time(satellites, str_time, Radius_of_Earth, bin_interval)
+        draw_RSO_histogram_at_time(RSOs, str_time, RADIUS_OF_EARTH, bin_interval)
+
+    if show_conjunctions_in_3D:
+        draw_conjunctions_in_3D(total_results)
